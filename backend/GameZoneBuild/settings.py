19a80419ae93e75aeea957089571076cb632eb8d
Futuree <uldashevshuhrat123@gmail.com>
2026-04-18 21:22:01 +0500

    Remove django-filters from INSTALLED_APPS - fix import error

--- backend/GameZoneBuild/settings.py
+++ backend/GameZoneBuild/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django-filters',
-    'corsheaders',
+        'corsheaders',
     'shop',
 ]
 
